21c5a04f3dab3cc1659eaeb39445c5f4fb5d64e3
Aaron Pang <pang.aaron56@gmail.com>
2014-02-10 21:29:57 -0800

    Initial Commit

---
+++ Flappang Bird/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  Flappang Bird
+//
+//  Created by Aaron Pang on 2/10/2014.
+//  Copyright __MyCompanyName__ 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
